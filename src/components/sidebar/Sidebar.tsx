c4403ba691cf6f2f74cf2725807f851aa55beea1
dlaperishvili <davitlaperishvili@gmail.com>
2023-02-27 15:24:44 +0400

    menu in progress

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import "./sidebar.scss"
+import SidebarMenu from './sidebarMenu/SidebarMenu'
 
 export default function Sidebar() {
   return (
-    <aside className='main_sidebar'>Sidebar</aside>
+    <aside className='main_sidebar'>
+      <div className="sidebar_container">
+        <div className="sidebar_header">
+          <div className="logo">
+            <a href="">
+              <i className="ri-dashboard-fill"></i>
+              <span>Dashboard Kit</span>
+            </a>
+          </div>
+        </div>
+        <SidebarMenu/>
+      </div>
+    </aside>
   )
 }
---
+++ src/components/sidebar/sidebarMenu/SidebarMenu.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import "./sidebarMenu.scss"
+
+export default function SidebarMenu() {
+  return (
+    <div className="sidebar_menu">
+      <ul>
+        <li>
+          <a href="">
+            <i className="ri-home-2-fill"></i>
+            <span>Overview</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <i className="ri-coupon-fill"></i>
+            <span>Tickets</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <i className="ri-lightbulb-fill"></i>
+            <span>Ideas</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <i className="ri-contacts-fill"></i>
+            <span>Contacts</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <i className="ri-user-3-fill"></i>
+            <span>Agents</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <i className="ri-article-fill"></i>
+            <span>Articles</span>
+          </a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="">
+            <i className="ri-settings-5-fill"></i>
+            <span>Settings</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <i className="ri-mail-volume-fill"></i>
+            <span>Subscription</span>
+          </a>
+        </li> 
+      </ul>
+    </div>
+  )
+}
